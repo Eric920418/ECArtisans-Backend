72f9353dce6fb1a06d758751ff12dfff3185a23f
Allison <theallisonlai@fakemail.com>
2024-04-20 15:54:12 +0800

    feat: 🎸  新增users的schema

--- models/user.js
+++ models/user.js
@@ -2,7 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-
+    name: String,
+    gender: String,
+    avatar: String,
+    birthday: String,
+    phone: String,
+    mail: String,
+    address: String,
+    passward: String,
+    otherPassward: String,
+    discount: Array,
+    spHistory: Array,
+    likeShop: Array,
+    collect: Array,
+    chat: Array,
 })
 
  const User = mongoose.model('Users', userSchema);
