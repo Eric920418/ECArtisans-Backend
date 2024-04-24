1fe10a3d601d6c8e60928236dbef9c255939e906
eric <26416387.re@gmail.com>
2024-04-24 15:16:39 +0800

    test: 💍 測試一下

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,108 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const Seller = require('../models/seller.js');
 
-/* GET home page. */
-router.get('/', (req, res, next)=> {
-  res.render('index', { title: 'Express' });
+//商家導覽
+router.get('/:sellerId/guide', async(req, res, next)=> {
+  const headers = {
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
+  }
+  try{
+    const seller = req.params.sellerId;
+    const thisShop = await Seller.findOne({_id:seller})
+    res.writeHead(200,headers);
+    res.write(JSON.stringify({
+        "status":"success",
+        thisShop
+    }))
+    res.end()
+  }catch(err){
+    res.writeHead(500, headers); 
+    res.end(JSON.stringify({
+        "status": "error",
+        "message": "Internal Server Error"
+    }));
+  }
+});
+//商家資訊
+router.get('/:sellerId/information', async(req, res, next)=> {
+  const headers = {
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
+  }
+  try{
+    const seller = req.params.sellerId;
+    const thisShop = await Seller.findOne({_id:seller})
+    res.writeHead(200,headers);
+    res.write(JSON.stringify({
+        "status":"success",
+        thisShop
+    }))
+    res.end()
+  }catch(err){
+    res.writeHead(500, headers); 
+    res.end(JSON.stringify({
+        "status": "error",
+        "message": "Internal Server Error"
+    }));
+  }
+});
+//訂單管理
+router.get('/:sellerId/orders', async(req, res, next)=> {
+  const headers = {
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
+  }
+  try{
+    const seller = req.params.sellerId;
+    const thisShop = await Seller.findOne({_id:seller})
+    res.writeHead(200,headers);
+    res.write(JSON.stringify({
+        "status":"success",
+        thisShop
+    }))
+    res.end()
+  }catch(err){
+    res.writeHead(500, headers); 
+    res.end(JSON.stringify({
+        "status": "error",
+        "message": "Internal Server Error"
+    }));
+  }
+});
+//單一訂單管理
+router.get('/:sellerId/order/:orderId', async(req, res, next)=> {
+  const headers = {
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
+  }
+  try{
+    const seller = req.params.sellerId;
+    const order = req.params.orderId;
+    const thisShop = await Seller.findOne({_id:seller},{order:order})
+    res.writeHead(200,headers);
+    res.write(JSON.stringify({
+        "status":"success",
+        thisShop
+    }))
+    res.end()
+  }catch(err){
+    res.writeHead(500, headers); 
+    res.end(JSON.stringify({
+        "status": "error",
+        "message": "Internal Server Error"
+    }));
+  }
 });
 
 module.exports = router;
