e99390ba65671ab15756cd685f808cd35ee7fbab
JonathanHsu0817 <00339028@email.ntou.edu.tw>
2024-05-19 20:38:09 +0800

    feat:1.商家折價券的api fixed:controller及coupon model錯誤顯示更改

--- controllers/shopControllers.js
+++ controllers/shopControllers.js
@@ -1,6 +1,7 @@
 const handleSuccess = require('../service/handleSuccess');
 const handleError = require('../service/handleError');
 const Products = require('../models/product');
+const Coupons = require('../models/coupon');
 const Seller = require('../models/seller');
 
 //商品相關
@@ -38,7 +39,7 @@ const products = {
 				.limit(qty)
 				.populate({
 					path: 'sellerOwned',
-					select: 'brand product',
+					select: 'brand discount',
 				});
 
 			const pagination = {
@@ -190,11 +191,15 @@ const products = {
 			if (!product) {
 				return res.status(404).json({
 					status: false,
-					message: '此商品沒被找到獲您無權刪除此商品',
+					message: '此商品沒被找到或您無權刪除此商品',
 				});
 			}
 
-			await Product.deleteOne({ _id: product_id });
+			await Products.deleteOne({ _id: product_id });
+
+			await Seller.findByIdAndUpdate(seller_id, {
+				$pull: { product: product_id },
+			});
 
 			res.json({
 				status: true,
@@ -210,4 +215,171 @@ const products = {
 	},
 };
 
-module.exports = products;
+//折價券相關
+const coupons = {
+	async getCoupons(req, res) {
+		try {
+			const sellerID = req.params.seller_id;
+			const page = parseInt(req.query.page, 10) || 1;
+			const qty = parseInt(req.query.qty, 12) || 12;
+
+			let query = { seller: sellerID };
+
+			const skip = (page - 1) * qty;
+			const totalCoupons = await Coupons.countDocuments({
+				seller: sellerID,
+			});
+			const totalPages = Math.ceil(totalCoupons / qty);
+
+			const coupons = await Coupons.find(query).skip(skip).limit(qty).populate({
+				path: 'seller',
+				select: 'brand discount',
+			});
+
+			const pagination = {
+				totalPages: totalPages,
+				currentPage: page,
+				hasPrev: page > 1,
+				hasNext: page < totalPages,
+			};
+
+			res.json({
+				status: true,
+				Coupons: coupons,
+				pagination: pagination,
+			});
+		} catch (err) {
+			handleError(err);
+		}
+	},
+	async getCoupon(req, res) {
+		const sellerID = req.params.seller_id;
+		const couponID = req.params.coupon_id;
+		const thisCoupon = await Coupons.find({
+			_id: couponID,
+			seller: sellerID,
+		}).populate({
+			path: 'seller',
+			select: 'brand coupon',
+		});
+
+		if (!thisCoupon) {
+			return res.status(404).json({
+				status: false,
+				message: '找不到此折價券',
+			});
+		}
+
+		handleSuccess(res, thisCoupon);
+	},
+	async createCoupon(req, res) {
+		try {
+			const seller = req.user._id;
+			const {
+				couponName,
+				startDate,
+				endDate,
+				type,
+				discountConditions,
+				percentage,
+				discountScope,
+				isEnabled,
+			} = req.body;
+
+			const newCoupon = await Coupons.create({
+				couponName,
+				startDate,
+				endDate,
+				type,
+				discountConditions,
+				percentage,
+				discountScope,
+				isEnabled,
+				seller,
+			});
+			// 更新卖家的折价券列表
+			await Seller.findByIdAndUpdate(seller, {
+				$push: { discount: newCoupon._id },
+			});
+			handleSuccess(res, newCoupon);
+		} catch (err) {
+			handleError(res, err);
+		}
+	},
+	async updateCoupon(req, res) {
+		try {
+			const couponID = req.params.coupon_id;
+			const seller = req.user._id;
+			const {
+				couponName,
+				startDate,
+				endDate,
+				type,
+				discountConditions,
+				percentage,
+				discountScope,
+				isEnabled,
+			} = req.body;
+
+			const updatedCoupon = await Coupons.findByIdAndUpdate(couponID, {
+				couponName,
+				startDate,
+				endDate,
+				type,
+				discountConditions,
+				percentage,
+				discountScope,
+				isEnabled,
+				seller,
+			});
+
+			if (!updatedCoupon) {
+				return res.status(404).json({
+					status: false,
+					message: '找不到產品',
+				});
+			}
+
+			handleSuccess(res, updatedCoupon);
+		} catch (err) {
+			handleError(res, err);
+		}
+	},
+	async deleteCoupon(req, res) {
+		const { seller_id, coupon_id } = req.params;
+		try {
+			const coupon = await coupon.findOne({
+				_id: coupon_id,
+				sellerOwned: seller_id,
+			});
+			if (!coupon) {
+				return res.status(404).json({
+					status: false,
+					message: '此折價券沒被找到或您無權刪除此商品',
+				});
+			}
+
+			await Coupon.deleteOne({ _id: coupon_id });
+
+			await Seller.findByIdAndUpdate(seller_id, {
+				$pull: { product: product_id },
+			});
+
+			res.json({
+				status: true,
+				message: '此商品成功被刪除',
+			});
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({
+				status: false,
+				message: 'Internal Server Error',
+			});
+		}
+	},
+};
+
+module.exports = {
+	products,
+	coupons,
+};
